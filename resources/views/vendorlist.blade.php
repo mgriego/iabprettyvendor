0d46a2a7b9b51035a7163c26c31224aa6dc458a9
Michael Griego <mike@griegocentral.com>
2018-05-17 16:44:07 -0500

    Initial commit

---
+++ resources/views/vendorlist.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>IAB Vendor List</title>
+    </head>
+    <body>
+        <h1>IAB Vendor List</h1>
+        <h3>Total Vendor Count: {{ count($vendorlist) }}</h3>
+        <table style="width: 100%;">
+            <tr>
+                <th>Vendor ID</th>
+                <th>Vendor Name (links to privacy policy)</th>
+                <th>Purposes</th>
+                <th>Legitimate Interest Purposes</th>
+                <th>Features</th>
+            </tr>
+            @foreach ($vendorlist as $id => $vendor)
+                @if ($id % 2 === 0)
+                <tr style="background-color: #eee;">
+                @else
+                <tr>
+                @endif
+                    <td>{{ $vendor['id'] }}</td>
+                    <td><a href="{{ $vendor['privacy'] }}">{{ $vendor['name'] }}</a></td>
+                    <td>
+                        <ul>
+                        @foreach ($vendor['purposes'] as $value)
+                            <li>{{ $value }}</li>
+                        @endforeach
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                        @foreach ($vendor['legintpurposes'] as $value)
+                            <li>{{ $value }}</li>
+                        @endforeach
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                        @foreach ($vendor['features'] as $value)
+                            <li>{{ $value }}</li>
+                        @endforeach
+                        </ul>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,97 @@
+<?php
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client as GuzzleHttpClient;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function (Request $request) {
+    $client = new GuzzleHTTPClient();
+    $response = $client->get('https://vendorlist.consensu.org/vendorlist.json');
+    $rawVendorList = json_decode($response->getBody()->getContents(), true);
+    $vendorList = [];
+    
+    foreach ($rawVendorList['vendors'] as $id => $vendor) {
+        $purposes = [];
+        foreach ($vendor['purposeIds'] as $purpose) {
+            if (isset($rawVendorList['purposes'][$purpose])) {
+                $purposes[] = e($rawVendorList['purposes'][$purpose]['name']);
+            } else {
+                $purposes[] = e('Unknown Purpose');
+            }
+        }
+        if (count($purposes) === 0) {
+            $purposes[] = 'None';
+        }
+        
+        $legitimateInterestPurposes = [];
+        foreach ($vendor['legIntPurposeIds'] as $purpose) {
+            if (isset($rawVendorList['purposes'][$purpose])) {
+                $legitimateInterestPurposes[] = e($rawVendorList['purposes'][$purpose]['name']);
+            } else {
+                $legitimateInterestPurposes[] = e('Unknown Purpose');
+            }
+        }
+        if (count($legitimateInterestPurposes) === 0) {
+            $legitimateInterestPurposes[] = 'None';
+        }
+        
+        $features = [];
+        foreach ($vendor['featureIds'] as $feature) {
+            if (isset($rawVendorList['features'][$feature])) {
+                $features[] = e($rawVendorList['features'][$feature]['name']);
+            } else {
+                $features[] = e('Unknown Feature');
+            }
+        }
+        if (count($features) === 0) {
+            $features[] = 'None';
+        }
+
+        $vendorList[] = [
+            'id' => $vendor['id'],
+            'name' => $vendor['name'],
+            'privacy' => $vendor['policyUrl'],
+            'purposes' => $purposes,
+            'legintpurposes' => $legitimateInterestPurposes,
+            'features' => $features,
+        ];
+    }
+
+    $sort = $request->query('sort');
+    $rev = $request->query('rev');
+
+    if ($sort !== 'name') {
+        $sort = 'id';
+    }
+
+    usort($vendorList, function($a, $b) use ($sort, $rev) {
+        $a = $a[$sort];
+        $b = $b[$sort];
+        
+        if ($sort === 'name') {
+            $a = strtolower($a);
+            $b = strtolower($b);
+        }
+
+        if ($a === $b) {
+            return 0;
+        } else if ($rev === '1') {
+            return ($a > $b) ? -1 : 1;
+        } else {
+            return ($a < $b) ? -1 : 1;
+        }
+    });
+    
+    return view('vendorlist', ['vendorlist' => $vendorList]);
+});
